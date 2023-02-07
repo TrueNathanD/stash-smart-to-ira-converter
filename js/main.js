c5b7a90cf75e20a7fdd077f359752eaf8ceef657
Nathan D Strzelczyk <truenathand@gmail.com>
2023-02-07 07:57:52 -0600

    Added HTML elements to catch JavaScript math

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 console.log("Hello, World.")
 
 function calculate() {
-
     /* Grabs the variable values from the input boxes */
-    var antHexaVal = parseFloat(document.getElementById("antHexaVal").value);
-    var batHexaVal = parseFloat(document.getElementById("batHexaVal").value);
-    var catHexaVal = parseFloat(document.getElementById("catHexaVal").value);
-    var dogHexaVal = parseFloat(document.getElementById("dogHexaVal").value);
-    var eelHexaVal = parseFloat(document.getElementById("eelHexaVal").value);
-    var foxHexaVal = parseFloat(document.getElementById("foxHexaVal").value);
+    let antHexaVal = parseFloat(document.getElementById("antHexaVal").value);
+    let batHexaVal = parseFloat(document.getElementById("batHexaVal").value);
+    let catHexaVal = parseFloat(document.getElementById("catHexaVal").value);
+    let dogHexaVal = parseFloat(document.getElementById("dogHexaVal").value);
+    let eelHexaVal = parseFloat(document.getElementById("eelHexaVal").value);
+    let foxHexaVal = parseFloat(document.getElementById("foxHexaVal").value);
+
+    /* Displays the total value entered by the user */
+    let totalHexaVal = (antHexaVal + batHexaVal + catHexaVal + dogHexaVal + eelHexaVal + foxHexaVal);
+    document.getElementById("totalHexaVal").innerHTML = totalHexaVal.toFixed(2);
 
     /* Distributes eelHexaVal and foxHexaVal out evenly among the other variables */
-    var distributedAmount = (eelHexaVal + foxHexaVal) / 4;
-    var antBoxVal = antHexaVal + distributedAmount;
-    var batBoxVal = batHexaVal + distributedAmount;
-    var catBoxVal = catHexaVal + distributedAmount;
-    var dogBoxVal = dogHexaVal + distributedAmount;
+    let distributedAmount = (eelHexaVal + foxHexaVal) / 4;
+    let antBoxVal = antHexaVal + distributedAmount;
+    let batBoxVal = batHexaVal + distributedAmount;
+    let catBoxVal = catHexaVal + distributedAmount;
+    let dogBoxVal = dogHexaVal + distributedAmount;
+
+    /* Displays the total value remaining */
+    let totalBoxVal = (antBoxVal + batBoxVal + catBoxVal + dogBoxVal);
+    document.getElementById("totalBoxVal").innerHTML = totalBoxVal.toFixed(2);
     
     /* Displays the new variable values */
     document.getElementById("antBoxVal").innerHTML = antBoxVal.toFixed(2);
@@ -24,23 +31,29 @@ function calculate() {
     document.getElementById("dogBoxVal").innerHTML = dogBoxVal.toFixed(2);
 
     /* Divides the new variable values by 20 (gets % of $20) */
-    var antBoxPer = antBoxVal / 20;
-    var batBoxPer = batBoxVal / 20;
-    var catBoxPer = catBoxVal / 20;
-    var dogBoxPer = dogBoxVal / 20;
-    document.getElementById("antBoxPer").innerHTML = antBoxPer.toFixed(2);
-    document.getElementById("batBoxPer").innerHTML = batBoxPer.toFixed(2);
-    document.getElementById("catBoxPer").innerHTML = catBoxPer.toFixed(2);
-    document.getElementById("dogBoxPer").innerHTML = dogBoxPer.toFixed(2);
+    let antBoxPer = antBoxVal / 20;
+    let batBoxPer = batBoxVal / 20;
+    let catBoxPer = catBoxVal / 20;
+    let dogBoxPer = dogBoxVal / 20;
+    document.getElementById("antBoxPer").innerHTML = antBoxPer.toFixed(2) * 100;
+    document.getElementById("batBoxPer").innerHTML = batBoxPer.toFixed(2) * 100;
+    document.getElementById("catBoxPer").innerHTML = catBoxPer.toFixed(2) * 100;
+    document.getElementById("dogBoxPer").innerHTML = dogBoxPer.toFixed(2) * 100;
+
+    /* Displays the total percent used in calculation */
+    document.getElementById("totalBoxPer").innerHTML = (antBoxPer + batBoxPer + catBoxPer + dogBoxPer).toFixed(2) * 100;
 
     /* Multiplies the new variable values that were divided by 20 by 115 (converts % to $ of $115) */
-    var antPerOf = antBoxPer * 115;
-    var batPerOf = batBoxPer * 115;
-    var catPerOf = catBoxPer * 115;
-    var dogPerOf = dogBoxPer * 115;
+    let antPerOf = antBoxPer * 115;
+    let batPerOf = batBoxPer * 115;
+    let catPerOf = catBoxPer * 115;
+    let dogPerOf = dogBoxPer * 115;
     document.getElementById("antPerOf").innerHTML = antPerOf.toFixed(2);
     document.getElementById("batPerOf").innerHTML = batPerOf.toFixed(2);
     document.getElementById("catPerOf").innerHTML = catPerOf.toFixed(2);
     document.getElementById("dogPerOf").innerHTML = dogPerOf.toFixed(2);
+
+    let totalPerOf = (antPerOf + batPerOf + catPerOf + dogPerOf)
+    document.getElementById("totalPerOf").innerHTML = totalPerOf.toFixed(2);
 }
 
